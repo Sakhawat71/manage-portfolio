e8bfcdccd60980ef5ada42dafba8b79c9114bf4c
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-05-20 18:03:15 +0600

    Add Navbar component: implement Navbar with user authentication links, dashboard navigation, and logout functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/components/shared/Navbar';
 import { Button } from '@/components/ui/button';
 import React from 'react';
 
 const HomePage = () => {
     return (
         <div>
+            <Navbar />
             <h1>Welcome to Home</h1>
             <Button> Click </Button>
         </div>
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { Button } from "../ui/button";
+import { HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
+import Link from "next/link";
+import { useUser } from "@/context/UserContext";
+import { useRouter } from "next/navigation";
+import { logoutUser } from "@/services/auth";
+
+
+
+export default function Navbar() {
+
+    const { user, setIsLoading, isLoading } = useUser();
+    console.log("in navbar", user);
+    // const pathname = usePathname();
+    const router = useRouter();
+
+    const handlelogout = () => {
+        logoutUser();
+        setIsLoading(true);
+        router.push("/");
+    };
+
+    return (
+        <header className="border-b w-full">
+            <div className="container flex justify-between items-center mx-auto h-16 px-3">
+
+                <Link href="/" className="text-2xl font-black flex items-center">
+                    Sakhawat
+                </Link>
+
+                <nav className="flex gap-2">
+
+                    <Link href="/">
+                        <Button variant="outline">
+                            <HomeIcon />
+                        </Button>
+                    </Link>
+
+                    {
+                        user ?
+                            <>
+                                <Link href='/dashboard'>
+                                    <Button
+                                        variant='outline'
+                                        className="rounded-full"
+                                    >Dashboard</Button>
+                                </Link>
+
+                                <Button
+                                    onClick={handlelogout}
+                                    disabled={isLoading}
+                                    variant='outline'
+                                    className="mx-auto flex items-center"
+                                >
+                                    <LogOutIcon />LogOut
+                                </Button>
+                            </>
+                            :
+                            <Link href='/login'>
+                                <Button
+                                    disabled={isLoading}
+                                    variant='outline'
+                                    className="flex items-center"
+                                ><LogInIcon /> Login</Button>
+                            </Link>
+                    }
+
+
+
+                </nav>
+            </div>
+        </header>
+    );
+}
