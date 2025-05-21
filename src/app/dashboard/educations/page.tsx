a9b6e35ee4c3263f73cee28c14a4baf515d75e5a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-05-21 13:42:06 +0600

    feat: add pages for projects, blogs, contacts, educations, experiences, and skills

---
+++ src/app/dashboard/educations/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EducationsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default EducationsPage;
---
+++ src/app/dashboard/experiences/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ExperiencesPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ExperiencesPage;
