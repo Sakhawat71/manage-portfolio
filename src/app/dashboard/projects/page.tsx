25ea0a2d34a22920d6c76a40d2695f64ed892022
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-05-24 17:14:02 +0600

    feat: implement ProjectsTable component and integrate project fetching in ProjectPage

--- src/app/dashboard/projects/page.tsx
+++ src/app/dashboard/projects/page.tsx
@@ -1,9 +1,16 @@
+import { ProjectsTable } from '@/components/modules/dashboard/projects/ProjectTable';
+import { getAllProjects } from '@/services/project';
 import React from 'react';
 
-const ProjectPage = () => {
+const ProjectPage = async () => {
+
+    const projects = await getAllProjects();
+    console.log(projects);
+
     return (
         <div>
             <h4>Projects page</h4>
+            <ProjectsTable />
         </div>
     );
 };
--- src/services/project/index.ts
+++ src/services/project/index.ts
@@ -1,12 +1,16 @@
 "use server";
 
 export const getAllProjects = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/project`, {
-        method: "GET",
-        cache: "no-store",
-    });
-    if (!res.ok) {
-        throw new Error("Failed to fetch projects");
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/api/projects`, {
+            method: "GET",
+            cache: "no-store",
+        });
+        if (!res.ok) {
+            throw new Error("Failed to fetch projects");
+        }
+        return await res.json();
+    } catch (error: any) {
+        return Error(error);
     }
-
 };
